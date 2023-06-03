6728afd84af149c63cd1ce658fb8f73061613b78
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-06-03 12:43:51 +0200

    Added utility to tailwind config file too, because fe.fyi website can't extract them out of the index.css file YET.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
+import plugin from "tailwindcss/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // In your own projects it's enough to add this only to the index.css file.
+    // In our case the preview editor on frontend.fyi struggles with extracting these
+    // classnames from index.css and generate the proper classnames in the fly.
+    // That's why in that case we've added the utilities here too, because they ARE picked up.
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".mask-legend": {
+          maskImage: `linear-gradient(
+            transparent,
+            black 45%,
+            black 55%,
+            transparent 100%
+          )`,
+        },
+      });
+    }),
+  ],
 };
